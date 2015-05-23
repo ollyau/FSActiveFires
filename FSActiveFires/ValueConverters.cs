672b88da37b8c0d16da79acfe8978007123caf45
Orion <ollyau@domain.tld>
2015-05-22 21:10:58 -0700

    Update informational text and value converters.

--- FSActiveFires/ValueConverters.cs
+++ FSActiveFires/ValueConverters.cs
@@ -1,35 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Windows.Data;
 
 namespace FSActiveFires {
-    class ConnectedTitleConverter : IValueConverter {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            return (bool)value ? "SimConnect: Connected" : "SimConnect: Disconnected";
+    public class BooleanConverter<T> : IValueConverter {
+        public BooleanConverter(T trueValue, T falseValue) {
+            True = trueValue;
+            False = falseValue;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-            throw new NotSupportedException();
+        public T True { get; set; }
+        public T False { get; set; }
+
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return value is bool && ((bool)value) ? True : False;
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return value is T && EqualityComparer<T>.Default.Equals((T)value, True);
         }
     }
 
-    class ConnectedButtonConverter : IValueConverter {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            return (bool)value ? "Disconnect" : "Connect";
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-            throw new NotSupportedException();
-        }
+    public sealed class InverseBooleanConverter : BooleanConverter<bool> {
+        public InverseBooleanConverter() :
+            base(false, true) { }
     }
 
-    public class InverseBooleanConverter : IValueConverter {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            return !(bool)value;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-            throw new NotSupportedException();
-        }
+    public sealed class BooleanToStringConverter : BooleanConverter<string> {
+        public BooleanToStringConverter() :
+            base("True", "False") { }
     }
 }
